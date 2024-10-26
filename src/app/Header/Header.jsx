33b36146e060c0f1b69068247d7280dbdc14abc0
19rajapandi <nitrajapandi2019@gmail.com>
2024-10-26 14:27:39 +0530

    new post2

--- src/app/Header/Header.jsx
+++ src/app/Header/Header.jsx
@@ -25,59 +25,62 @@ const Header = () => {
   ];
 
   return (
-    <div className='flex justify-between items-center h-24 max-w-full mx-auto px-4 text-black shadow-md'>
-      {/* Logo */}
-      <div onClick={() => goToPage('/')} className='cursor-pointer'>
-        <img src="img/LOGO.svg" alt="Logo" className='w-40 h-20' />
-      </div>
-
-      {/* Desktop Navigation */}
-      <ul className='hidden md:flex font-medium'>
-        {navItems.map(item => (
-          <li
-            key={item.id}
-            className='p-4 hover:bg-gray-200 rounded-xl m-2 cursor-pointer duration-300 hover:text-green-400'
-            onClick={() => goToPage(item.path)}
-            role="menuitem"
-            aria-label={item.text}
-          >
-            {item.icon}
-            {item.text}
-          </li>
-        ))}
-      </ul>
-
-      {/* Mobile Navigation Icon */}
-      <div onClick={handleNav} className='block md:hidden cursor-pointer' aria-label="Toggle Navigation">
-        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
-      </div>
-
-      {/* Mobile Navigation Menu */}
-      <ul
-        className={`${
-          nav ? 'z-50 fixed' : 'fixed left-[-100%]'
-        } md:hidden top-0 left-0 w-[60%] h-full border-r border-r-gray-900 bg-white ease-in-out duration-500`}
-      >
-        {/* Mobile Logo */}
+    <>
+      <title>Tekno Spot</title>
+      <link rel="icon" href="img/logo .png" />
+      <div className='flex justify-between items-center h-24 max-w-full mx-auto px-4 text-black shadow-md'>
+        {/* Logo */}
         <div onClick={() => goToPage('/')} className='cursor-pointer'>
-          <img src="img/LOGO.svg" alt="Logo" className='w-40 h-20 mb-10 mt-5' />
+          <img src="img/LOGO.svg" alt="Logo" className='w-40 h-20' />
         </div>
 
-        {/* Mobile Navigation Items */}
-        {navItems.map(item => (
-          <li
-            key={item.id}
-            className='p-4 border-b rounded-xl font-medium duration-300 hover:text-green-400 cursor-pointer border-gray-600'
-            onClick={() => goToPage(item.path)} // This will close the mobile menu
-            role="menuitem"
-            aria-label={item.text}
-          >
-            {item.icon}
-            {item.text}
-          </li>
-        ))}
-      </ul>
-    </div>
+        {/* Desktop Navigation */}
+        <ul className='hidden md:flex font-medium'>
+          {navItems.map(item => (
+            <li
+              key={item.id}
+              className='p-4  m-2 cursor-pointer duration-300 hover:text-green-500'
+              onClick={() => goToPage(item.path)}
+              role="menuitem"
+              aria-label={item.text}
+            >
+              {item.icon}
+              {item.text}
+            </li>
+          ))}
+        </ul>
+
+        {/* Mobile Navigation Icon */}
+        <div onClick={handleNav} className='block md:hidden cursor-pointer' aria-label="Toggle Navigation">
+          {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+        </div>
+
+        {/* Mobile Navigation Menu */}
+        <ul
+          className={`${nav ? 'z-50 fixed' : 'fixed left-[-100%]'
+            } md:hidden top-0 left-0 w-[60%] h-full border-r border-r-gray-900 bg-white ease-in-out duration-500`}
+        >
+          {/* Mobile Logo */}
+          <div onClick={() => goToPage('/')} className='cursor-pointer'>
+            <img src="img/LOGO.svg" alt="Logo" className='w-40 h-20 mb-10 mt-5' />
+          </div>
+
+          {/* Mobile Navigation Items */}
+          {navItems.map(item => (
+            <li
+              key={item.id}
+              className='p-4 border-b rounded-xl font-medium duration-300 hover:text-green-400 cursor-pointer border-gray-600'
+              onClick={() => goToPage(item.path)} // This will close the mobile menu
+              role="menuitem"
+              aria-label={item.text}
+            >
+              {item.icon}
+              {item.text}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 };
 
